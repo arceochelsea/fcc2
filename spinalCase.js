9a34d4aca122f7d42f9660b24a0fe3ca872fe7eb
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-07-17 23:59:01 -0400

    Spinal Case

---
+++ spinalCase.js
@@ -0,0 +1,8 @@
+// Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+
+function spinalCase(str) {
+
+    return str.replace(/[\s_]/g, '-').replace(/[a-z])([A-Z])/g, '$1-$2').toLowerCase();
+}
+
+console.log(spinalCase('This Is Spinal Tap'));
