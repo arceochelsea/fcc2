b047b81b556532bf973140b1352726d6102e14f9
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-07-22 18:47:20 -0400

    Convert HTML

---
+++ convertHTMLEntities.js
@@ -0,0 +1,12 @@
+// Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+
+function convertHTML(str) {
+  
+    return str;
+  }
+  
+  console.log(convertHTML("Dolce & Gabbana"));
+  //should return "Dolce &amp; Gabbana".
+
+  //save them as variables 
+  //if x.includes(y) then replace(z)??
