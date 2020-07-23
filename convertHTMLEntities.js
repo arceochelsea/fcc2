d8a4d29afcb6ca490ffbfdc343a543141d424103
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-07-23 15:44:47 -0400

    Convert HTML Entities

--- convertHTMLEntities.js
+++ convertHTMLEntities.js
@@ -1,12 +1,12 @@
 // Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
 
 function convertHTML(str) {
-  
+    let characters = {'&':'&amp;','<':'&lt;','>':'&gt;', '"':'&quot;', "'":'&apos;'};
+    str = str.replace(/&|<|>|'|"/g, m => characters[m]);
     return str;
-  }
+    }
   
   console.log(convertHTML("Dolce & Gabbana"));
   //should return "Dolce &amp; Gabbana".
 
-  //save them as variables 
-  //if x.includes(y) then replace(z)??
+  //If you want to replace multiple characters you can call the String.prototype.replace() with the replacement argument being a function that gets called for each match. All you need is an object representing the character mapping which you will use in that function.
